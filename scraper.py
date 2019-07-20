08427926f7eef6ce26ace716927c0df793179706
Toivo Toivo <toivo1996@gmail.com>
2019-07-20 22:43:52 +0200

    First commit

---
+++ scraper.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import json
+import csv
+
+def get_all_urls():
+	# iterate over the different urls
+	for page in range(1):
+		url = 'https://www.namauto.com/cars/?order=price&page={}&per_page=12'.format(page)
+
+		html = urlopen(url)
+		bs = BeautifulSoup(html.read(), 'lxml')
+
+		cars = bs.findAll('div', {'class': 'cycle off'})
+
+		for car in cars:
+			res = get_car_data(car.a['href'])
+			write_to_csv(res)
+
+def get_car_data(url):
+
+	full_url = 'https://www.namauto.com{}'.format(url)
+	res = urlopen(full_url)
+	data = BeautifulSoup(res.read(), 'lxml')
+
+	car = []
+
+	car.append(full_url)
+	car.append(data.find('div', {'class': 'price-now'}).select('span.value')[0].get_text().strip())
+	for prop in data.find(True, {'class': 'overview-data-standard'}).find_all(True, {'class': 'value'}):
+		car.append(prop.getText().strip())
+
+	return car
+
+def write_to_csv(car):
+	with open("namauto.csv", 'a', newline='') as f: 
+		writer = csv.writer(f)
+		writer.writerows([car])
+		print('Added...')
+
+if __name__ == '__main__':
+	get_all_urls()
