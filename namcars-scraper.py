43a1217d5aecba7482d4f9cbbef68a0c85fb49d7
Toivo Toivo <toivo1996@gmail.com>
2020-01-12 10:29:20 +0200

    Commit

--- namcars-scraper.py
+++ namcars-scraper.py
@@ -3,7 +3,6 @@ from urllib.request import urlopen
 
 
 def get_all_urls():
-
 	for page in range(1, 100):
 		url = 'https://www.namcars.net/index.php?module=cars&filters=1&type=used_cars,demo,new_cars&page={}&lang=en'.format(page)
 
@@ -12,14 +11,16 @@ def get_all_urls():
 
 		for carlink in bs.find('div', {'class': 'itemsContainer'}).find_all('a', {'class': 'link'}):
 			href = carlink.get('href')
-			get_link_data(href)
-			break
+			get_car_data(href)
+			print('------------------------------------------HERE-----------------------------------------')
 
-def get_link_data(url):
+def get_car_data(url):
 	base = 'https://www.namcars.net{}'.format(url)
 
 	car = urlopen(base)
 	res = BeautifulSoup(car.read(), 'lxml')
+	car_attr = []
+	car_attr_keys = ['reg-year', 'mileage', 'engine-size', 'fuel-type', 'transmission', 'drive', 'finance']
 
 	print(res.find('div', {'class': 'detail_left'}).find('span', itemprop="price").getText().strip()) # item price
 	for header in res.find('div', {'class': 'title-box'}).find_all('span'):
@@ -29,7 +30,10 @@ def get_link_data(url):
 			print(prop + ": " + val)
 
 	for spec in res.find('div', 'car-specs').find_all('div', {'class': 'row'}):
-		print(spec.getText().strip())	
+		car_attr.append(spec.getText().strip())
+
+	print(car_attr[:len(car_attr_keys)])
+	print(car_attr_keys)
 
 if __name__ == '__main__':
 	get_all_urls()
